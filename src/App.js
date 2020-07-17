af29b427bd7e2d8ec54a628fcd4c6f09b0319d7d
mat_anderson <matandersonlv@hotmail.com>
2020-07-17 16:27:42 -0700

    changed the default React port from 3000 to 3001 to avoid conflicts with Rails API

--- src/App.js
+++ src/App.js
@@ -3,28 +3,7 @@ import MainHeader from './components/MainHeader'
 import IndexNavBar from './components/IndexNavBar'
 
 class App extends Component {
-
-  componentDidMount() {
-    fetch('https://stickers-js-api-backend.herokuapp.com/api/v1/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-         Accept: 'application/json'
-       },
-       body: JSON.stringify({
-         // form inputs were stored in vars at the top of DOMContentLoaded event handler (callback Fn)
-         user: {
-          name: 'Taco',
-          email: 'email@email.com',
-          password: 'password',
-          avatar: 'https://cdn3.vectorstock.com/i/1000x1000/47/92/redhead-male-character-avatar-userpic-vector-17104792.jpg'
-         }
-       })
-    })
-    .then((r) => r.json())
-    .then(newUserJSON => console.log(newUserJSON))
-  }
-
+  
   render() {
     return (
       <div className="App">
